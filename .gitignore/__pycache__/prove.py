ee9c9924fc77e604efc0efe06745a489a6893a89
iunezon <grmgrg000@gmail.com>
2024-06-14 22:49:46 +0200

    change .gitignore

--- .gitignore/__pycache__/prove.py
+++
@@ -1,3 +0,0 @@
-text = "This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)"
-splits = [i+1 for i in range(len(text) - 1) if text[i:i+2] in " *"] + [i+2 for i in range(len(text) - 1) if text[i:i+2] in "* "]
-print(text[25:34])
